50ebef6c2ffb1d1cce91bc28e5b13c2a8d5a2215
Djmalevsky <djmalevsky@gmail.com>
2022-10-21 17:27:29 -0400

    pp

--- Project/src/Message.java
+++ Project/src/Message.java
@@ -1,9 +1,9 @@
 package Project.src;
 
 public class Message {
-    private int msgLength;
-    private char msgType;
-    private int msgPayload;
+    int msgLength;
+    char msgType;
+    int msgPayload;
 
     public final char NO_PAYLOAD = 0xFF;
     public final char CHOKE = 0;
@@ -14,10 +14,41 @@ public class Message {
     public final char BITFIELD = 5;
     public final char REQUEST = 6;
     public final char PIECE = 7;
+    public Message()
+    {
 
+    }
     public Message(int msgLength, char msgType) {
         this.msgLength = msgLength;
         this.msgType = msgType;
+    }
+
+    public char getMessageType() {
+        return msgType;
+    }
+
+    public int getMessageLength() {
+        return msgLength;
+    }
+
+    public int getMessagePayload() {
+        return msgPayload;
+    }
+    class Choke extends Message {
+        public Choke() {
+            this.msgType = CHOKE;
+
+            this.msgLength = 0;
+
+            this.msgPayload = 0;
+
+        }
+
+        public void sendmsg()
+        {
+            HandShake.getMessage();
+        }
+
     }
 
 }
